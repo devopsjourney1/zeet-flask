b6be9dc3c12d49c495de903505e7fb7ada0f1b97
devopsjourney1 <34145011+devopsjourney1@users.noreply.github.com>
2022-11-09 21:16:25 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    html = f"<h1>Hello World!</h1>"
+    return html
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5000)
